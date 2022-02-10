28970fe95a06868339a97830b266536c7c27b8ec
Mutlu Çalkan <mutlu.calkan@gmail.com>
2022-02-10 16:18:02 +0300

    Initial Commit

---
+++ GradeCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradeCalculator
+//
+//  Created by macbook on 10.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
